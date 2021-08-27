44fc8f913dee3932d6f1c02ffc1608a0f48aa97f
lion-oldriver <w879443a72cj2i@gmail.com>
2021-08-27 09:12:15 +0900

    [finish]js1

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-alert('Hello,world')
+let int1 = 4
+let int2 = 7
+
+alert('足し算' + int1 + '+' + int2 + '=' + (int1 + int2));
+alert('引き算' + int1 + '-' + int2 + '=' + (int1 - int2));
+alert('掛け算' + int1 + '*' + int2 + '=' + (int1 * int2));
+alert('割り算' + int1 + '/' + int2 + '=' + (int1 / int2));
